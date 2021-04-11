df0fe6c33c5fb99baccb28b37de4d8d922dd5e0b
GitSparTV <developspartv@gmail.com>
2021-04-11 16:39:59 +0300

    Additional unit tests

--- tests/lexer_test.lua
+++ tests/lexer_test.lua
@@ -78,6 +78,233 @@ local TokenMap = {
 	["~"] = tokens["tilde"],
 }
 
+-- Testing internals
+do
+	-- Setup and Lexer.Next()
+	do
+		assert_cmp(Lexer.EOF, -1, "Lexer.EOF is not -1")
+
+		local lex = Lexer.Setup("")
+
+		assert_cmp(lex.buf, "", "Input buffer is not saved to lex.buf")
+		assert_cmp(lex.size, 0, "lex.size of the input buffer is wrong")
+		assert_cmp(lex.offset, 1, "Incorrect initial lex.offset")
+		assert_cmp(lex.c, Lexer.EOF, "lex.c is not EOF")
+		assert_cmp(lex.linenumber, 1, "lex.linenumber is not initialized properly")
+		assert_cmp(lex.columnnumber, 0, "lex.columnnumber is not initialized properly")
+		assert_cmp(lex.lastline, 1, "lex.lastline is not initialized properly")
+		assert_cmp(#lex.strbuf, 64, "lex.strbuf is not preallocated")
+		assert_cmp(lex.strbufsize, 0, "lex.strbufsize is not initialized properly")
+		assert_cmp(lex.strbufresult, "", "lex.strbufresult is not initialized properly")
+		assert_cmp(lex.tok, -1, "lex.tok is not initialized properly")
+		assert_cmp(lex.lookahead, tokens.eof, "lex.lookahead is not EOF")
+
+		Lexer.Next(lex)
+
+		assert_cmp(lex.buf, "", "lex.buf changed at runtime")
+		assert_cmp(lex.size, 0, "lex.size changed at runtime")
+		assert_cmp(lex.offset, 1, "lex.offset moved even when there is no more characters to read")
+		assert_cmp(lex.c, Lexer.EOF, "lex.c is not EOF")
+		assert_cmp(lex.linenumber, 1, "lex.linenumber should not be changed")
+		assert_cmp(lex.columnnumber, 0, "lex.columnnumber should not be changed")
+		assert_cmp(lex.lastline, 1, "lex.lastline should not be changed")
+		assert_cmp(lex.strbufsize, 0, "lex.strbufsize should not be changed")
+		assert_cmp(lex.strbufresult, "", "lex.strbufresult should not be changed")
+		assert_cmp(lex.tok, tokens.eof, "lex.tok is not EOF")
+		assert_cmp(lex.lookahead, tokens.eof, "lex.lookahead is not EOF")
+	end
+
+	-- lex.linenumber, lex.columnnumber, lex.lastline
+	do
+		local lex = Lexer.Setup("      \t\t\t\t\t\t\n\n\n     ")
+
+		Lexer.Next(lex)
+
+		assert_cmp(lex.linenumber, 4, "lex.linenumber is not counted properly")
+		assert_cmp(lex.columnnumber, 5, "lex.columnnumber is not counted properly")
+		assert_cmp(lex.lastline, 1, "lex.lastline is not counted properly")
+
+		Lexer.Next(lex)
+
+		assert_cmp(lex.lastline, 4, "lex.lastline is not counted properly")
+	end
+
+	do
+		local lex = Lexer.Setup(string.rep(" \t\v\f ", 10))
+
+		Lexer.Next(lex)
+
+		assert_cmp(lex.columnnumber, 50, "lex.columnnumber is not counted properly")
+	end
+
+	-- lex:Next()
+	do
+		local str = "abcdefg"
+		local lex = Lexer.Setup(str)
+
+		assert_cmp(lex.c, string.byte(str, 1), "lex.c is not initialized with first character")
+
+		for k = 2, #str do
+			assert_cmp(lex(), string.byte(str, k), "Wrong read in lex.с in character #", k)
+		end
+
+		assert_cmp(lex(), Lexer.EOF, "lex.c is not set to EOF when the all characters are read")
+		assert_cmp(lex(), Lexer.EOF, "lex.c should be always EOF when the all characters are read")
+	end
+
+	-- lex:Save(), lex:SaveN(), lex:SaveNext(), lex:ConcatStrBuffer(), lex:ResetStrBuffer()
+	do
+		local str = "1234567890"
+		local lex = Lexer.Setup(str)
+
+		do
+			lex:Save("q")
+			lex:Save("w")
+			lex:Save("e")
+			lex:Save("r")
+			lex:Save("t")
+			lex:Save("y")
+
+			assert_cmp(lex:ConcatStrBuffer(), "qwerty", "lex:ConcatStrBuffer() returns incorrect string")
+
+			lex:ResetStrBuffer()
+
+			assert_cmp(lex.strbufsize, 0, "lex.strbufsize is not reset")
+		end
+
+		do
+			lex:SaveN(1)
+			lex:SaveN(2)
+			lex:SaveN(3)
+			lex:SaveN(4)
+			lex:SaveN(5)
+			lex:SaveN(6)
+
+			assert_cmp(lex:ConcatStrBuffer(), "\1\2\3\4\5\6", "lex:ConcatStrBuffer() returns incorrect string")
+
+			lex:ResetStrBuffer()
+
+			assert_cmp(lex.strbufsize, 0, "lex.strbufsize is not reset")
+		end
+
+		do
+			local counter = 0
+
+			while lex:SaveNext() ~= Lexer.EOF do
+				counter = counter + 1
+
+				assert_cmp(lex.strbufsize, counter, "lex.strbufsize is not counted properly")
+				assert_cmp(lex:ConcatStrBuffer(), str:sub(1, counter), "lex:ConcatStrBuffer() returns incorrect string")
+			end
+
+			lex:ResetStrBuffer()
+
+			assert_cmp(lex.strbufsize, 0, "lex.strbufsize is not reset")
+		end
+	end
+
+	-- lex:SkipEq()
+	do
+		do
+			local lex = Lexer.Setup("[")
+
+			assert_cmp(lex:SkipEq(), -1, "lex:SkipEq() is not counting properly")
+		end
+
+		do
+			local lex = Lexer.Setup("[[")
+
+			assert_cmp(lex:SkipEq(), 0, "lex:SkipEq() is not counting properly")
+		end
+
+		do
+			local lex = Lexer.Setup("[===")
+
+			assert_cmp(lex:SkipEq(), -4, "lex:SkipEq() is not counting properly")
+		end
+
+		do
+			local lex = Lexer.Setup("[======[")
+
+			assert_cmp(lex:SkipEq(), 6, "lex:SkipEq() is not counting properly")
+		end
+	end
+
+	-- lex:LongComment()
+	do
+		local lex = Lexer.Setup("[=[\n12345]]678]===]90\n]=]")
+
+		lex:LongComment(1)
+
+		assert_cmp(lex.strbuf[0], false, "lex:LongComment() should not save into lex.strbuf")
+		assert_cmp(lex.linenumber, 3, "lex:LongComment() doesn't increment lex.linenumber")
+		assert_cmp(lex.c, Lexer.EOF, "lex.c is not EOF")
+	end
+
+	-- lex:LongString()
+	do
+		local lex = Lexer.Setup("[===[\n12345]]6]===78]===]90\n]===]")
+		lex()
+		lex()
+		lex()
+		lex()
+		lex:LongString(3)
+
+		assert_cmp(lex.linenumber, 2, "lex:LongComment() doesn't increment lex.linenumber")
+		assert_cmp(lex.c, string.byte("9"), "lex.c didn't end on next character after ]")
+		assert_cmp(lex:ConcatStrBuffer(), [===[
+12345]]6]===78]===], "Incorrect saving in lex:LongString()")
+	end
+
+	-- lex:Number()
+	do
+		do
+			local lex = Lexer.Setup("1234567890")
+			lex:Number()
+			assert_cmp(lex:ConcatStrBuffer(), "1234567890", "lex:Number() doesn't read numbers properly")
+		end
+
+		do
+			local lex = Lexer.Setup("123e-1")
+			lex:Number()
+			assert_cmp(lex:ConcatStrBuffer(), "123e-1", "lex:Number() doesn't read numbers properly")
+		end
+
+		do
+			local lex = Lexer.Setup("123.1")
+			lex:Number()
+			assert_cmp(lex:ConcatStrBuffer(), "123.1", "lex:Number() doesn't read numbers properly")
+		end
+
+		do
+			local lex = Lexer.Setup("00001")
+			lex:Number()
+			assert_cmp(lex:ConcatStrBuffer(), "00001", "lex:Number() doesn't read numbers properly")
+		end
+
+		do
+			local lex = Lexer.Setup("0xAbCdEfp-1")
+			lex:Number()
+			assert_cmp(lex:ConcatStrBuffer(), "0xAbCdEfp-1", "lex:Number() doesn't read numbers properly")
+		end
+
+		do
+			local lex = Lexer.Setup("0XABCDE-3")
+			lex:Number()
+			assert_cmp(lex:ConcatStrBuffer(), "0XABCDE", "lex:Number() doesn't read numbers properly")
+		end
+	end
+
+	do
+		local lex = Lexer.Setup("\n\r\n\n\r\r\r")
+
+		for k = 2, 5 do
+			lex:Newline()
+			assert_cmp(lex.linenumber, k, "Incorrect lex.linenumber incrementing")
+		end
+	end
+end
+
 -- Testing lexing separately
 do
 	local function GetToken(script)
@@ -89,20 +316,13 @@ do
 
 	local function TestToken(script, token)
 		local a = GetToken(script)
+
 		assert_cmp(tokennames[a] or a, tokennames[token], "Token mismatch.")
 	end
 
 	TestToken("", tokens.eof)
 	TestToken("           ", tokens.eof)
 	TestToken("      \t\t\t\t\t\t\n\n\n     ", tokens.eof)
-
-	do
-		local lex = Lexer.Setup("      \t\t\t\t\t\t\n\n\n     ")
-		Lexer.Next(lex)
-		assert_cmp(lex.linenumber, 4, "lex.linenumber is not counted properly")
-		assert_cmp(lex.columnnumber, 5, "lex.columnnumber is not counted properly")
-	end
-
 	TestToken("1234.67890", tokens.number)
 	TestToken("1", tokens.number)
 	TestToken("0xabcdFE", tokens.number)
