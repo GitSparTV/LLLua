1a97971f18ca30d31eec0a8ef7bec8c236ed0231
GitSparTV <developspartv@gmail.com>
2021-04-12 16:17:47 +0300

    Fixed assert hint not being displayed. Fixed missing newline when no hint specified

--- tests/assert_advanced.lua
+++ tests/assert_advanced.lua
@@ -4,15 +4,18 @@ function assert_fatal(bool)
 	isfatal = bool
 end
 
-function assert(condition, hint, ...)
+function assert(condition, ...)
 	if not condition then
 		if isfatal then
-			error(hint or "assertion failed!", 2)
+			local concat = table.concat({...})
+			error(... and ". " .. concat or "assertion failed!", 2)
 		else
 			local func = debug.getinfo(2, "lS")
-			io.write(func.short_src, ":", func.currentline, ": assertion failed!\n")
+			io.write(func.short_src, ":", func.currentline, ": assertion failed!")
+			if ... then io.write(" ", ...) end
+			io.write("\n")
 
-			return condition, hint, ...
+			return condition, ...
 		end
 	end
 
@@ -27,7 +30,8 @@ function assert_cmp(a, b, ...)
 		else
 			local func = debug.getinfo(2, "lS")
 			io.write(func.short_src, ":", func.currentline, ": assertion failed! \"", tostring(a), " ~= ", tostring(b), "\".")
-			if ... then io.write(" ", ...) io.write("\n") end
+			if ... then io.write(" ", ...) end
+			io.write("\n")
 		end
 	end
 end
